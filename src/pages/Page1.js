1485ca9592f9cea8f9d8206316ce0c3bd2c79781
toku0223 <kei.nakanotoku@gmail.com>
2022-09-28 20:55:22 +0900

    main

---
+++ src/pages/Page1.js
@@ -0,0 +1,13 @@
+import { Link } from 'gatsby'
+import React from 'react'
+
+const FirstPage = () => {
+  return (
+    <div>
+      <h1>Page1</h1>
+      <Link to="/">HOME</Link>
+    </div>
+  )
+}
+
+export default FirstPage
